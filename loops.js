a81811cb92feac7dd22d2afa53cf8d2a16847ccb
Austin Williams <austinq.williams@gmail.com>
2019-02-28 21:37:50 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+array =[]
+
+function forloop(array) {
+  if (i === 1) {
+  var sting = "I am 1 strange loop." 
+  
+ }  else if (i === 2) {
+ string  = "I am ${i} strange loops."
+ }
+} else { 
+  for (let i = 0; i < 25; i++) {
+ } 
+}
